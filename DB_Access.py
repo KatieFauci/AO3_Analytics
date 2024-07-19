60cfc4af4968914b6ca2d92623a456e57e2faeeb
Katie <66881113+KatieFauci@users.noreply.github.com>
2024-07-19 16:51:51 -0400

    cleaned up queries

--- DB_Access.py
+++ DB_Access.py
@@ -1,11 +1,8 @@
 import sqlite3
 
-def create_database():
-    conn = sqlite3.connect('works.db')
-    c = conn.cursor()
-    
-    # Create works table
-    c.execute('''CREATE TABLE works (
+# Define global SQL strings
+# Database Setup
+CREATE_WORKS_TABLE = '''CREATE TABLE IF NOT EXISTS works (
                     id INTEGER PRIMARY KEY,
                     title TEXT,
                     author_id INTEGER NOT NULL,
@@ -25,63 +22,34 @@ def create_database():
                     num_collections INTEGER DEFAULT 0,
                     collections_link TEXT,
                     FOREIGN KEY(author_id) REFERENCES authors(id)
-                )'''
-              )
-    
-    # Create author table
-    c.execute('''CREATE TABLE IF NOT EXISTS authors (
-                    id INTEGER PRIMARY KEY,
-                    author TEXT UNIQUE
-              )'''
-              )
-    
-    # Create tags table
-    c.execute('''CREATE TABLE IF NOT EXISTS tags (
+)'''
+CREATE_AUTHORS_TABLE = '''CREATE TABLE IF NOT EXISTS authors (id INTEGER PRIMARY KEY, author TEXT UNIQUE)'''
+CREATE_TAGS_TABLE = '''CREATE TABLE IF NOT EXISTS tags (
                     id INTEGER PRIMARY KEY,
                     tag TEXT UNIQUE,
                     tag_class TEXT,
-                    is_ship BOOLEAN DEFAULT 0
-              )'''
-              )
-    
-   # Create work_tags table
-    c.execute('''CREATE TABLE IF NOT EXISTS work_tags (
+                    is_ship BOOLEAN DEFAULT 0)'''
+CREATE_WORK_TAG_RELATION_TABLE = '''CREATE TABLE IF NOT EXISTS work_tags (
                     work_id INTEGER,
                     tag_id INTEGER,
                     PRIMARY KEY (work_id, tag_id),
                     FOREIGN KEY(work_id) REFERENCES works(id),
                     FOREIGN KEY(tag_id) REFERENCES tags(id)
               )'''
-              )
-    
-    # Create fandoms table
-    c.execute('''CREATE TABLE IF NOT EXISTS fandoms (
-                    id INTEGER PRIMARY KEY,
-                    fandom TEXT UNIQUE
-              )'''
-              )
-    
-    # Create work_fandoms table
-    c.execute('''CREATE TABLE IF NOT EXISTS work_fandoms (
+CREATE_FANDOMS_TABLE = '''CREATE TABLE IF NOT EXISTS fandoms (id INTEGER PRIMARY KEY, fandom TEXT UNIQUE)'''
+CREATE_WORK_FANDOM_RELATION_TABLE = '''CREATE TABLE IF NOT EXISTS work_fandoms (
                     work_id INTEGER,
                     fandom_id INTEGER,
                     PRIMARY KEY (work_id, fandom_id),
                     FOREIGN KEY(work_id) REFERENCES works(id),
                     FOREIGN KEY(fandom_id) REFERENCES fandoms(id)
               )'''
-              )  
-
-    # Create series table
-    c.execute('''CREATE TABLE IF NOT EXISTS series (
+CREATE_SERIES_TABLE = '''CREATE TABLE IF NOT EXISTS series (
                     series_id INTEGER PRIMARY KEY,
                     series_name TEXT UNIQUE,
                     series_link TEXT UNIQUE
               )'''
-              )
-
-  
-    # Create work_series table
-    c.execute('''CREATE TABLE IF NOT EXISTS work_series (
+CREATE_WORK_SERIES_RELATION_TABLE = '''CREATE TABLE IF NOT EXISTS work_series (
                     work_id INTEGER,
                     series_id INTEGER,
                     part_number INTEGER,
@@ -89,34 +57,78 @@ def create_database():
                     FOREIGN KEY(work_id) REFERENCES works(id),
                     FOREIGN KEY(series_id) REFERENCES series(series_id)
               )'''
-             )
+
+# Inserting to tables
+INSERT_AUTHOR = "INSERT INTO authors (author) VALUES (?)"
+INSERT_WORK = "INSERT INTO works (title, author_id, rating, word_count, last_visited, date_published, lang, completed_chapters, total_chapters, completed, comments, kudos, bookmarks, hits) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+INSERT_TAG = "INSERT INTO tags (tag, tag_class, is_ship) VALUES (?, ?, ?)"
+INSERT_WORK_TAG_RELATION = "INSERT INTO work_tags (work_id, tag_id) VALUES (?, ?)"
+INSERT_FANDOM = "INSERT INTO fandoms (fandom) VALUES (?)"
+INSERT_WORK_FANDOM_RELATION = "INSERT INTO work_fandoms (work_id, fandom_id) VALUES (?, ?)"
+INSERT_SERIES = "INSERT OR IGNORE INTO series (series_name, series_link) VALUES (?, ?)"
+INSERT_WORK_SERIES_RELATION = "INSERT OR IGNORE INTO work_series (work_id, series_id) VALUES (?, ?)"
+
+# Selects
+SELECT_WORK_AUTHOR_TITLE_RATING = "SELECT id FROM works WHERE title = ? AND author_id = ? AND rating = ?"
+SELECT_AUTHOR_BY_NAME = "SELECT id FROM authors WHERE author = ?"
+SELECT_TAG_WITH_CLASS = "SELECT id FROM tags WHERE tag = ? AND tag_class = ?"
+SELECT_WORK_TAG_RELATION = "SELECT * FROM work_tags WHERE work_id = ? AND tag_id = ?"
+SELECT_FANDOM_ID_BY_FANDOM = "SELECT id FROM fandoms WHERE fandom = ?"
+SELECT_WORK_FANDOM_RELATION = "SELECT * FROM work_fandoms WHERE work_id = ? AND fandom_id = ?"
+SELECT_SERIES_ID_BY_NAME = "SELECT series_id FROM series WHERE series_name = ?"
+
+
+
+def create_connection():
+    conn = sqlite3.connect('works.db')
+    return conn
+
+def create_database():
+    conn = sqlite3.connect('works.db')
+    c = conn.cursor()
+    
+    # Create Tables
+    c.execute(CREATE_WORKS_TABLE)
+    c.execute(CREATE_AUTHORS_TABLE)
+    c.execute(CREATE_TAGS_TABLE)
+    c.execute(CREATE_WORK_TAG_RELATION_TABLE)
+    c.execute(CREATE_FANDOMS_TABLE)
+    c.execute(CREATE_WORK_FANDOM_RELATION_TABLE)  
+    c.execute(CREATE_SERIES_TABLE)
+    c.execute(CREATE_WORK_SERIES_RELATION_TABLE)
 
     conn.commit()
     conn.close()
 
-def add_author(author_name, c):
-    # Insert into authors table
-    c.execute("SELECT id FROM authors WHERE author = ?", (author_name,))
+def add_author(author_name, conn):
+    conn = create_connection()
+    c = conn.cursor()
+    
+    # Use the global variable for the SELECT query
+    c.execute(SELECT_AUTHOR_BY_NAME, (author_name,))
     author_id = c.fetchone()
-
+    
     if author_id is None:
         try:
-            c.execute("INSERT INTO authors (author) VALUES (?)", (author_name,))
+            # Use the global variable for the INSERT query
+            c.execute(INSERT_AUTHOR, (author_name,))
+            conn.commit()  # Commit the changes after inserting a new author
             author_id = c.lastrowid
-        except Exception as e:
-            print(f'Error Adding Author: {e}, AUTHOR_ID = {author_id}')
-        return author_id
-    
-    return author_id[0];
+        except sqlite3.Error as e:
+            print(f'Error Adding Author: {e}')  
+        else:
+            return author_id
+    else:
+        return author_id[0]
 
 def add_work(data, author_id, c):
     # Insert into works table
-    c.execute("SELECT id FROM works WHERE title = ? AND author_id = ? AND rating = ?", 
+    c.execute(SELECT_WORK_AUTHOR_TITLE_RATING, 
               (data.title, author_id, data.rating))
     work_id = c.fetchone()
 
     if work_id is None:
-        c.execute("INSERT INTO works (title, author_id, rating, word_count, last_visited, date_published, lang, completed_chapters, total_chapters, completed, comments, kudos, bookmarks, hits) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        c.execute(INSERT_WORK,
                   (data.title, author_id, data.rating, data.word_count, data.last_visited, data.published, data.language, data.completed_chapters, data.total_chapters, data.completed, data.comments, data.kudos, data.bookmarks, data.hits))
         work_id = c.lastrowid
         return work_id
@@ -126,51 +138,51 @@ def add_work(data, author_id, c):
 def add_work_tags(tags, work_id, c):
     # Insert into tags table and story_tags table
     for tag in tags:
-        c.execute("SELECT id FROM tags WHERE tag = ? AND tag_class = ?", (tag['Tag'], tag['TagClass']))
+        c.execute(SELECT_TAG_WITH_CLASS, (tag['Tag'], tag['TagClass']))
         tag_id = c.fetchone()
         
         if tag_id is None:
             # Check if the tag is a ship
             is_ship = 1 if tag['TagClass'] == 'relationships' and '/' in tag['Tag'] else 0
-            c.execute("INSERT INTO tags (tag, tag_class, is_ship) VALUES (?, ?, ?)", (tag['Tag'], tag['TagClass'], is_ship))
+            c.execute(INSERT_TAG, (tag['Tag'], tag['TagClass'], is_ship))
             tag_id = c.lastrowid
         else:
             tag_id = tag_id[0]
 
-        c.execute("SELECT * FROM work_tags WHERE work_id = ? AND tag_id = ?", (work_id, tag_id))
+        c.execute(SELECT_WORK_TAG_RELATION, (work_id, tag_id))
         if c.fetchone() is None:
-            c.execute("INSERT INTO work_tags (work_id, tag_id) VALUES (?, ?)", (work_id, tag_id))
+            c.execute(INSERT_WORK_TAG_RELATION, (work_id, tag_id))
 
 def add_work_fandoms(fandoms, work_id, c):
     # Insert into fandoms table and work_fandoms table
     for fandom in fandoms:
-        c.execute("SELECT id FROM fandoms WHERE fandom = ?", (fandom,))
+        c.execute(SELECT_FANDOM_ID_BY_FANDOM, (fandom,))
         fandom_id = c.fetchone()
         
         if fandom_id is None:
-            c.execute("INSERT INTO fandoms (fandom) VALUES (?)", (fandom,))
+            c.execute(INSERT_FANDOM, (fandom,))
             fandom_id = c.lastrowid
         else:
             fandom_id = fandom_id[0]
 
-        c.execute("SELECT * FROM work_fandoms WHERE work_id = ? AND fandom_id = ?", (work_id, fandom_id))
+        c.execute(SELECT_WORK_FANDOM_RELATION, (work_id, fandom_id))
         if c.fetchone() is None:
-            c.execute("INSERT INTO work_fandoms (work_id, fandom_id) VALUES (?, ?)", (work_id, fandom_id))
+            c.execute(INSERT_WORK_FANDOM_RELATION, (work_id, fandom_id))
 
 def insert_series(series_name, series_link, work_id):
-    conn = sqlite3.connect('works.db')
+    conn = create_connection()
     c = conn.cursor()
     
     # Insert into series table only if the series_name does not exist
-    c.execute("INSERT OR IGNORE INTO series (series_name, series_link) VALUES (?, ?)", (series_name, series_link))
+    c.execute(INSERT_SERIES, (series_name, series_link))
     series_id = c.lastrowid
 
     if series_id == 0:
-        c.execute("SELECT series_id FROM series WHERE series_name = ?", (series_name,))
+        c.execute(SELECT_SERIES_ID_BY_NAME, (series_name,))
         series_id = c.fetchone()[0]
 
     # Insert into work_collections table to link the work to the series
-    c.execute("INSERT OR IGNORE INTO work_series (work_id, series_id) VALUES (?, ?)", (work_id, series_id))
+    c.execute(INSERT_WORK_SERIES_RELATION, (work_id, series_id))
     
     conn.commit()
     conn.close()
@@ -179,7 +191,7 @@ def insert_series(series_name, series_link, work_id):
 
 
 def insert_work_into_database(data):
-    conn = sqlite3.connect('works.db')
+    conn = create_connection()
     c = conn.cursor()
 
     author_id = add_author(data.author, c)
@@ -193,7 +205,7 @@ def insert_work_into_database(data):
 
 def search_works_by_tag(database_file, tag):
     # Open the SQLite database file
-    conn = sqlite3.connect(database_file)
+    conn = create_connection()
     cursor = conn.cursor()
 
     # Create the SQL query with the provided tag
